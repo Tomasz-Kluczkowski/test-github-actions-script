c5e647d3613f491e0c4edd23b5dc154180f9d16e
Tomasz-Kluczkowski <tomasz.kluczkowski@flexciton.com>
2022-08-28 19:49:21 +0100

    first test of the pipeline.

---
+++ scripts/upload_plan.js
@@ -0,0 +1,115 @@
+module.exports = async (github, context) => {
+    console.log('Test text');
+    // Get pull requests that are open for current ref.
+    // const pullRequests = await github.rest.pulls.list({
+    //     owner: context.repo.owner,
+    //     repo: context.repo.repo,
+    //     state: 'open',
+    //     head: `${context.repo.owner}:${context.ref.replace('refs/heads/', '')}`
+    // })
+    //
+    // // Set issue number for following calls from context (if on pull request event) or from above variable.
+    // const issueNumber = context.issue.number || pullRequests.data[0].number
+    //
+    // // Retrieve existing bot comments for the PR
+    // const {data: comments} = await github.rest.issues.listComments({
+    //     owner: context.repo.owner,
+    //     repo: context.repo.repo,
+    //     issue_number: issueNumber,
+    // })
+    // const botComment = comments.find(comment => {
+    //     return comment.user.type === 'Bot' && comment.body.includes('Terraform Plan')
+    // })
+    //
+    // // Prepare format of the comment
+    // // const output = `#### Terraform Plan 📖\`${outcome}\`
+    // const output = `#### Terraform Plan 📖
+    //       <hr>
+    //
+    //       *Pusher: @${github.actor}*
+    //       *Action: \`${github.event_name}\`*
+    //       *Workflow: \`${github.workflow}\`*
+    //       *Commit: \`${github.sha}\`*
+    //
+    //       <details><summary>Show Plan</summary>
+    //
+    //       \`\`\`\n
+    //       ${terraformPlan}
+    //       \`\`\`
+    //
+    //       </details>`;
+    //
+    // // If we have a comment, update it, otherwise create a new one
+    // if (botComment) {
+    //     github.rest.issues.updateComment({
+    //         owner: context.repo.owner,
+    //         repo: context.repo.repo,
+    //         comment_id: botComment.id,
+    //         body: output
+    //     })
+    // } else {
+    //     github.rest.issues.createComment({
+    //         issue_number: issueNumber,
+    //         owner: context.repo.owner,
+    //         repo: context.repo.repo,
+    //         body: output
+    //     })
+    // }
+}
+
+
+// // Get pull requests that are open for current ref.
+// const pullRequests = await github.rest.pulls.list({
+//     owner: context.repo.owner,
+//     repo: context.repo.repo,
+//     state: 'open',
+//     head: `${context.repo.owner}:${context.ref.replace('refs/heads/', '')}`
+// })
+//
+// // Set issue number for following calls from context (if on pull request event) or from above variable.
+// const issueNumber = context.issue.number || pullRequests.data[0].number
+//
+// // Retrieve existing bot comments for the PR
+// const {data: comments} = await github.rest.issues.listComments({
+//     owner: context.repo.owner,
+//     repo: context.repo.repo,
+//     issue_number: issueNumber,
+// })
+// const botComment = comments.find(comment => {
+//     return comment.user.type === 'Bot' && comment.body.includes('Terraform Plan')
+// })
+//
+// // Prepare format of the comment
+// const output = `#### Terraform Plan 📖\`${{steps.terraform-plan.outcome}}\`
+//           <hr>
+//
+//           *Pusher: @${{github.actor}}*
+//           *Action: \`${{github.event_name}}\`*
+//           *Workflow: \`${{github.workflow}}\`*
+//           *Commit: \`${{github.sha}}\`*
+//
+//           <details><summary>Show Plan</summary>
+//
+//           \`\`\`\n
+//           ${{env.TERRAFORM_PLAN}}
+//           \`\`\`
+//
+//           </details>`;
+//
+// // If we have a comment, update it, otherwise create a new one
+// if (botComment) {
+//     github.rest.issues.updateComment({
+//         owner: context.repo.owner,
+//         repo: context.repo.repo,
+//         comment_id: botComment.id,
+//         body: output
+//     })
+// } else {
+//     github.rest.issues.createComment({
+//         issue_number: issueNumber,
+//         owner: context.repo.owner,
+//         repo: context.repo.repo,
+//         body: output
+//     })
+// }
+//
